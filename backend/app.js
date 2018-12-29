97c4909cadc62a16282aa6b8ee9476dbfd6e898f
Shreyas Patil <shreyas@Shreyass-MacBook-Pro.local>
2018-12-28 22:52:20 -0800

    Adding node/express layer

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+	next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+	const post = req.body;
+	console.log(post);
+	res.status(201).json({
+		message: 'Post Added'
+	});
+});
+
+app.get('/api/posts', (req, res, next) => {
+	const posts = [
+		{
+			id: '1',
+			title: 'First Post',
+			content: 'First Content'
+		},
+		{
+			id: '2',
+			title: 'Second Post',
+			content: 'Second Content'
+		}
+	];
+
+	res.status(200).json({
+		message: 'Posts fetched successfully',
+		posts: posts
+	});
+});
+
+module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {
 	MatToolbarModule,
 	MatExpansionModule
 } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 		MatCardModule,
 		MatButtonModule,
 		MatToolbarModule,
-		MatExpansionModule
+		MatExpansionModule,
+		HttpClientModule
 	],
 	providers: [],
 	bootstrap: [ AppComponent ]
